a64a1399ca62b4d93b6aadda6d671906e7b973dd
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-05-19 09:56:01 +0300

    add a function to js to enable onclick

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+
+document.getElementById("checkout-buttton").onclick = function () {
+    location.href = "../pages/add-to-cartpage2.html";
+};
+
+document.getElementById("confirm-payment1").onclick = function () {
+    location.href = "../pages/add-to-cartpage3.html";
+};
+
