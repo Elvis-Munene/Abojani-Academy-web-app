3a3430152c206bb9c283b40480f87beca0f8fe3f
ndurivin <ndurivo@gmail.com>
2022-05-19 15:16:59 +0300

    Added two new pages and additional fixes on courses pages and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 
 document.getElementById("btn-buy").onclick = function () {
     location.href = "../pages/add-to-cart.html";
+}
+
+document.getElementById("btn-watch").onclick = function () {
+    location.href = "../pages/coursespage2.html";
+}
 
 
 
